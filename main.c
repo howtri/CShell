6c9ad6360cbd255e39325289911286ed0a19e4cb
Tristan Howell <howeltri@os1.engr.oregonstate.edu>
2021-10-23 12:29:18 -0700

    modularize parsing

--- main.c
+++ main.c
@@ -10,6 +10,14 @@ struct shellStructures
     int lastStatus; // set to 0 at the start
 };
 
+struct process
+{
+    char* name;
+    int pid;
+    int exitStatus;
+    struct process* next;
+};
+
 struct command 
 {
     char* binary;
@@ -24,6 +32,76 @@ void exitShell()
     // kill any other procs or jobs started
 }
 
+void parseCommand(char* input, struct command* currCommand)
+{
+    char* saveptr = input;
+    char* token;
+    
+    bool firstProcessed = false;
+    bool argsInit = false;
+
+    while ((token = strtok_r(saveptr, " \n", &saveptr)))
+    {
+        // check for comments and pull the binary
+        if (!firstProcessed)
+        {
+            firstProcessed = true;
+            // skip over any comments
+            if (strcmp("#", token) == 0)
+            {
+                continue;
+            }
+            // store our binary name
+            currCommand->binary = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(currCommand->binary, token);
+            continue;
+        }
+
+
+        // initialize our first arg, after this we reallocate for all additional tokens
+        if (!argsInit)
+        {
+            argsInit = true;
+            currCommand->arguments = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(currCommand->arguments, token);
+            //strcat(currCommand->arguments, " ");
+            continue;
+        }
+
+        currCommand->arguments = realloc(currCommand->arguments, strlen(currCommand->arguments) + strlen(token) + 1);
+        strcat(currCommand->arguments, token);
+        //strcat(currCommand->arguments, " ");
+    }
+
+    currCommand->background = false;
+    if (currCommand->arguments != NULL)
+    {
+        char lastChar = currCommand->arguments[strlen(currCommand->arguments) - 3];
+        printf("last val in args is %c\n", lastChar);
+        if (lastChar == '&')
+        {
+            printf("Yeah looking backgroundish to me\n");
+            currCommand->background = true;
+        }
+    }
+}
+
+void handleBuiltIns(struct command* current)
+{
+    if (strcmp(current->binary, "cd") == 0)
+    {
+        printf("cd\n");
+    }
+    else if (strcmp(current->binary, "status") == 0)
+    {
+        printf("status\n");
+    }
+    else if (strcmp(current->binary, "exit") == 0)
+    {
+        printf("exiting\n");
+        exit(0);
+    }
+}
 /*
 *   entry point and launch menu function
 */
@@ -41,9 +119,6 @@ int main(int argc, char* argv[])
         ssize_t lineSize = 0;
         lineSize = getline(&line, &len, stdin);
 
-        char* saveptr = line;
-        char* token;
-
         if (!lineSize)
         {
             printf("Empty yo");
@@ -52,47 +127,14 @@ int main(int argc, char* argv[])
 
         // create our struct for the command we'll process
         struct command* currCommand = malloc(sizeof(struct command));
-        bool firstProcessed = false;
-        bool argsInit = false;
-
-        while ((token = strtok_r(saveptr, " ", &saveptr)))
-        {
-            printf("%s the token\n", token);
-            // check for comments and pull the binary
-            if (!firstProcessed) 
-            {
-                firstProcessed = true;
-                // skip over any comments
-                if (strcmp("#", token) == 0)
-                {
-                    continue;
-                }
-                // store our binary name
-                currCommand->binary = calloc(strlen(token) + 1, sizeof(char));
-                strcpy(currCommand->binary, token);
-                continue;
-            }
-
-
-            // initialize our first arg, after this we reallocate for all additional tokens
-            if (!argsInit)
-            {
-                argsInit = true;
-                currCommand->arguments = calloc(strlen(token) + 1, sizeof(char));
-                strcpy(currCommand->arguments, token);
-                strcat("\0");
-                continue;
-            }
-
-            currCommand->arguments = realloc(currCommand->arguments, strlen(currCommand->arguments) + strlen(token) + 1);
-            strcat(currCommand->arguments, "%s\0", token);
-
-            // someway to check if last one is & for background?
-        }
-
-        printf("%s for our command struct", currCommand->binary);
-        // strtok if first arg = # continue
+        parseCommand(line, currCommand);
         free(line);
+        
+        printf("binary: %s\n", currCommand->binary);
+        printf("arguments: %s\n", currCommand->arguments);
+        
+        handleBuiltIns(currCommand); // need to pass shell also if we need to close procs or return exit code
+
     }
     return EXIT_SUCCESS;
 }
